2d0a87723c7982cdff8c26e116a0109326ccefed
chariskogkos <ckok@intracom-telecom.com>
2018-08-02 14:56:25 +0300

    add class DBHandler

---
+++ Todo/src/todo/DBHandler.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package todo;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+import static javafx.application.Platform.exit;
+/**
+ *
+ * @author ckok
+ */
+public class DBHandler { 
+    
+   private Connection con;
+
+    DBHandler() {
+        try {
+            String dbURL = "jdbc:sqlite:C:\\Users\\ckok\\Downloads\\Todo\\Todo.db";
+            con = DriverManager.getConnection(dbURL);
+        } catch (SQLException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Database Error");
+            alert.setHeaderText(ex.getMessage());
+            alert.setContentText(ex.toString());
+
+            alert.showAndWait();
+            exit();
+        }
+    } 
+}
